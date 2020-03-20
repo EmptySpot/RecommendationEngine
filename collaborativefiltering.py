2fe78c635a2f5ee330506a80fa810ed33559d145
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2020-03-20 19:21:05 +0100

    De filters

--- collaborativefiltering.py
+++ collaborativefiltering.py
@@ -2,94 +2,94 @@ import mysql.connector
 from mysql.connector import Error
 
 mySQLConnection = mysql.connector.connect(host='localhost',
-                                          database='huwebshop',
+                                          database='...',           #Database naam
                                           user='root',
-                                          password='SlacksP@ss41!')
+                                          password='...')           #Vul je wachtwoord in
 cursor = mySQLConnection.cursor()
 
 
 def createcollabtable():
-    global limit                #Globale limiet, de code kan elk getal aan. Alleen duurt dat voor elke test run te lang waardoor een limiet handig was.
-    limit = (500,)
-    cursor.execute("DROP TABLE IF EXISTS profielgezien")
+    global limit                                                            #Globale limiet, de code kan elk getal aan. Pas hieronder het getal aan als je alles wilt doen. Dit was alleen voor snelheid tijdens tests
+    limit = 500
+    cursor.execute("DROP TABLE IF EXISTS profielgezien")                    #Aanmaak en droppen van alle tabellen
     cursor.execute("CREATE TABLE profielgezien (profid VARCHAR(255) PRIMARY KEY, kindofcust VARCHAR(255), recommendsforitem1 VARCHAR(255) NULL, recommendsforitem2 VARCHAR(255) NULL, recommendsforitem3 VARCHAR(255) NULL, recommendsforitem4 VARCHAR(255) NULL, recommendsforitem5 VARCHAR(255) NULL, recommendsforitem6 VARCHAR(255) NULL, recommendsforitem7 VARCHAR(255) NULL, recommendsforitem8 VARCHAR(255) NULL, recommendsforitem9 VARCHAR(255) NULL, recommendsforitem10 VARCHAR(255) NULL)")
     cursor.execute("DROP TABLE IF EXISTS colrecommendaties")
     cursor.execute("CREATE TABLE colrecommendaties (profid VARCHAR(255) PRIMARY KEY, recommendsforitem1 VARCHAR(255) NOT NULL, recommendsforitem2 VARCHAR(255) NOT NULL, recommendsforitem3 VARCHAR(255) NOT NULL, recommendsforitem4 VARCHAR(255) NOT NULL)")
 
-def resultaatlooper(resultaatproduct, productlijst):
+def resultaatlooper(resultaatproduct, productlijst):                    #Hier checked de code de resultaten in een looper
     for j in resultaatproduct:
-        product = ''.join(j)
-        if product not in productlijst:
+        product = ''.join(j)                                            #j is een product uit resultaatproduct, oftewel een productid, alleen is het nog een tuple.
+        if product not in productlijst:                                 #product is een productid die is gekocht, hiermee kijkt de code naar soortgelijken mensen, maar zolang hij niet al in de lijst staat.
             productlijst.append(product)
         if len(productlijst) == 11:
             break
-    if len(productlijst) > 3:
-        while len(productlijst) != 12:
-            productlijst.append("null")
+    if len(productlijst) > 3:                                           #productlijst is hoeveel producten dit persoon heeft bekeken, alleen zitten hier profielid en segment al in. Waardoor het limiet op 3 moet zitten
+        while len(productlijst) != 12:                                  #Het limiet zit op 3 om te zorgen dat mensen die 2 producten hebben bekeken alleen inzitten
+            productlijst.append("null")                                 #om te zorgen dat alles ingevuld wordt, ook al maak ik het nullable. Om crashes te voorkomen vul ik de overige aan met null
         mySql_insert_query3 = "INSERT INTO profielgezien (profid, kindofcust, recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        cursor.execute(mySql_insert_query3, (tuple(productlijst)))
+        cursor.execute(mySql_insert_query3, (tuple(productlijst)))      #Liefst wou ik alle MySQL queries buiten de main code halen, als dat geen problemen zou leveren was dat gebeurt. Echter een hele hoop errors later werkt dat niet.
         mySQLConnection.commit()
 
 def profielbekijker(i):
-    sql_selecter = ("SELECT prodid FROM profiles_previously_viewed WHERE profid = %s LIMIT %s", limit)
-    profielid = (i[1],)
-    cursor.execute(sql_selecter,(profielid))
-    resultaatproduct = cursor.fetchall()
+    sql_selecter = ("SELECT prodid FROM profiles_previously_viewed WHERE profid = %s LIMIT %s")
+    profielid = [i[1], limit]                                           #i[1] is iemands profielid, limiet blijft het limiet van bovenaan.
+    cursor.execute(sql_selecter, (profielid))
+    resultaatproduct = cursor.fetchall()                                #Alle productids die dat profiel i[1] heeft bekeken
     productlijst = [i[1], i[0]]
-    if resultaatproduct != []:
+    if resultaatproduct != []:                                          #Als persoon geen producten heeft bekeken, dan gaat de code door naar het volgende persoon. Anders gaat de code naar resultaatlooper om de resultaten te gebruiken.
         resultaatlooper(resultaatproduct, productlijst)
 
-def nogmeerrec(nieuwequery, run, i, recommendaties):
-    mySql_query = "SELECT recommendsforitem1, recommendsforitem2 FROM profielgezien WHERE %s IN (recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10) OR %s in(recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10)"
+def nogmeerrec(nieuwequery, run, i, recommendaties):                    #Nogmeerrec kijkt alleen naar prodids en zoekt naar andere mensen, andere mensen die ook dat product hebben bekeken en naar wat hun hebben bekeken.
+    mySql_query = "SELECT recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10 FROM profielgezien WHERE %s IN (recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10) OR %s in(recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10)"
     cursor.execute(mySql_query, (nieuwequery))
-    replyervan = cursor.fetchall()
-    run = 1
+    replyervan = cursor.fetchall()                                      #Alle recente producten die recent bij elkaar zijn bekeken.
+    run += 1                                                            #Om te voorkomen dat de code een loop maakt wanneer er een persoon is die 2 producten heeft gekocht waar niemand tot bijna niemand ook eentje van heeft gekocht.
     recommendatiesmaken(replyervan, run, i, recommendaties)
 
-def recommendatiesmaken(replyervan, run, i, recommendaties):
+def recommendatiesmaken(replyervan, run, i, recommendaties):            #Hier maken we de recommendaties aan voor een persoon, gebaseerd op alle informatie uit nogmeerrec en collabrec.
     for item in replyervan:
-        for itemwithinitem in item:
-            if itemwithinitem != i[2] and itemwithinitem != i[3] and itemwithinitem != 'null':
+        for itemwithinitem in item:                                     #De reply levert meerdere lists aan, dus moet ik de informatie uit de list met lists halen, waardoor ik hierdoor door elk item heenga
+            if itemwithinitem != i[2] and itemwithinitem != i[3] and itemwithinitem != 'null': #Voorkomen dat iemand recommendaties krijgt die ie al heeft bekeken of niet bestaan
                 recommendaties.append(itemwithinitem)
-            if len(recommendaties) == 4:
+            if len(recommendaties) == 4:                                #Voorkomen dat er te veel recommendaties zijn.
                 break
-    if len(recommendaties) != 4:
-        nieuwequery = tuple([i[2], i[3]])
+    if len(recommendaties) != 4:                                        #Bij te weinig informatie gaat de code verder zoeken
         if run == 0:
-            nogmeerrec(nieuwequery, run, i, recommendaties)
-    else:
+            nieuwequery = tuple([i[2], i[3]])
+            nogmeerrec(nieuwequery, run, i, recommendaties)             #Als collabrec te weinig recommendaties kan maken, dan gaan we door naar nogmeerrec om het aan te vullen
+    else:                                                               #Bij genoeg recommendaties slaat de code het op onder je profielid met 4 productids erachter
         mySql_insert_query3 = "INSERT INTO colrecommendaties (profid, recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4) VALUES (%s, %s, %s, %s, %s)"
         recommendaties.insert(0, i[0])
         cursor.execute(mySql_insert_query3, (tuple(recommendaties)))
         mySQLConnection.commit()
 
 
-def collabrec():
+def collabrec():                                                        #Beetje hetzelfde als nogmeerrec, alleen kijken we hier naar dat de segments hetzelfde zijn om het gedrag erbij te halen.
     cursor.execute("SELECT profid, kindofcust, recommendsforitem1, recommendsforitem2 FROM profielgezien")
     colabres = cursor.fetchall()
     for i in colabres:
-        querylist = list([i[1], i[2], i[3]])
+        querylist = list([i[1], i[2], i[3]])                            #segment (wat voor klant het persoon is), een item die het persoon ooit heeft bekeken en nog een item die het persoon heeft bekeken
         mySql_query = "SELECT recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10 FROM profielgezien WHERE kindofcust = %s AND %s IN (recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10) OR %s in(recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4, recommendsforitem5, recommendsforitem6, recommendsforitem7, recommendsforitem8, recommendsforitem9, recommendsforitem10)"
         cursor.execute(mySql_query, (tuple(querylist)))
         replyervan = cursor.fetchall()
-        recommendaties = []
-        run = 0
+        recommendaties = [] #Waar alle recommendaties ingaan
+        run = 0 #infinite loop voorkomen
         recommendatiesmaken(replyervan, run, i, recommendaties)
 
-def segment():
+def startensegment():                                                   #De start voor segment en producten te selecteren
     gebruikteids = []
-    cursor.execute("SELECT segment, profid FROM sessions WHERE segment NOT IN ('Bouncer', 'Leaver') LIMIT %s", limit)
+    cursor.execute("SELECT segment, profid FROM sessions WHERE segment NOT IN ('Bouncer', 'Leaver') LIMIT %s", (limit,)) #Een persoon die vaker terug komt zal naast een bouncer en / of een leaver ook wat anders zijn.
     resultaatseg = cursor.fetchall()
     for i in resultaatseg:
-        if i[0] == ('',) or i[0] == ('') or i[1] in gebruikteids:
+        if i[0] == ('',) or i[0] == ('') or i[1] in gebruikteids:       #Niet hetzelfde persoon of een niet bestaand profielid
             continue
         else:
-            gebruikteids.append(i[1])
-            profielbekijker(i)
-    if (mySQLConnection.is_connected()):
+            gebruikteids.append(i[1])                                   #Profielid die al gebruikt zijn
+            profielbekijker(i)                                          #Waar we alle informatie gaan bekijken per profiel
+    if (mySQLConnection.is_connected()):                                #Om de MySQL goed af te sluiten naderhand
         cursor.close()
         mySQLConnection.close()
 
-createcollabtable()
-segment()
-collabrec()
+createcollabtable()                                                     #Eerst de table in de database aanmaken
+startensegment()                                                        #In de eerste table alles stoppen wat een persoon heeft bekeken en wat voor klant het is
+collabrec()                                                             #Hierna gaat de tweede table aan de gang om te kijken wat dat persoon heeft bekeken en wat soortgelijke mensen hebben bekeken.
--- contentfiltering.py
+++ contentfiltering.py
@@ -7,6 +7,8 @@ mySQLConnection = mysql.connector.connect(host='localhost',
                                           password='SlacksP@ss41!')
 cursor = mySQLConnection.cursor()
 
+#De sql query
+#https://stackoverflow.com/questions/28383546/retrieve-all-ids-where-name-like-current-name-in-one-query/28383840#28383840
 sql_select_query = """select id,
         (SELECT GROUP_CONCAT(id SEPARATOR ', ')
         FROM products
@@ -15,66 +17,66 @@ sql_select_query = """select id,
         FROM products
         WHERE name = %s"""
 
-def createtable():
+def createtable(): #Table aanmaken voor recommendaties
     cursor.execute("DROP TABLE IF EXISTS recommendaties")
     cursor.execute("CREATE TABLE recommendaties (itemid VARCHAR(255) PRIMARY KEY, recommendsforitem1 VARCHAR(255) NOT NULL, recommendsforitem2 VARCHAR(255) NOT NULL, recommendsforitem3 VARCHAR(255) NOT NULL, recommendsforitem4 VARCHAR(255) NOT NULL)")
 
 def desqlding(vergelijking, originelenaam):
-    cursor.execute(sql_select_query, (('%' + vergelijking + '%'), originelenaam, originelenaam))
+    cursor.execute(sql_select_query, (('%' + vergelijking + '%'), originelenaam, originelenaam)) #% ervoor en erna om te zorgen dat er wordt gekeken wat in vergelijking staat, ergens in de productnaam staat. Originele naam is wat het product niet mag zijn en de andere ori naam is dat hij daaronder word opgeslagen
     record = cursor.fetchall()
     return record
 
 def eindlocatie(recommendsvoordit, recomlist):
     mySql_insert_query2 = "INSERT INTO recommendaties (itemid, recommendsforitem1, recommendsforitem2, recommendsforitem3, recommendsforitem4) VALUES (%s, %s, %s, %s, %s)"
     gelimiteerderecommends = [recommendsvoordit]
-    teller = 0
+    teller = 0                              #Om te voorkomen dat er teveel recommendaties zijn
     for item in recomlist:
         gelimiteerderecommends.append(item)
         teller += 1
         if teller == 4:
             break
-    gelimiteerderecommends = tuple(gelimiteerderecommends)
-    cursor.execute(mySql_insert_query2, gelimiteerderecommends)
+    gelimiteerderecommends = tuple(gelimiteerderecommends) #recommends als tuple
+    cursor.execute(mySql_insert_query2, gelimiteerderecommends) #insert statement
     mySQLConnection.commit()
 
-def getLaptopDetail(originelenaam, vergelijking, recomlist):
-    try:
+def recommendatiemaken(originelenaam, vergelijking, recomlist):
+    try:                                    #Wanneer een duplicate key inzit, deze wordt opgevangen door de try except
         record = desqlding(vergelijking, originelenaam)
-        a = record[0][1]
-        if a == None:
-            b = [0]
-        else:
-            b = str(a).split(', ')
-        for i in b:
+        meerdereprodid = record[0][1]       #Zorgen dat elke product eruit wordt gepakt
+        if meerdereprodid == None:          #Er kan niets in meerdereprodid zitten, om errors te voorkomen
+            elkeprodid = [0]
+        else:                               #Als er wel wat in stond, dan staat het als tuple, wat fout is
+            elkeprodid = str(meerdereprodid).split(', ')
+        for i in elkeprodid:
             if i not in recomlist and i != 0:
                 recomlist.append(i)
-        if len(recomlist) < 4:
+        if len(recomlist) < 4:              #Als er geen 4 producten vindbaar zijn met de volledige naam, dan split ik op het eerste woord van de naam. Als het product een merk heeft, dan is de merk vaak het eerste woord.
             if ' ' in vergelijking:
                 vergelijking = originelenaam.split(' ')[0]
             else:
-                vergelijking = vergelijking [:len(vergelijking)//2]
-            getLaptopDetail(originelenaam, vergelijking, recomlist)
+                vergelijking = vergelijking [:len(vergelijking)//2] #Als er geen spatie inzit, oftewel maar 1 woord is. Of als er zelfs met alleen het eerste woord geen 4 recommendaties zijn dan wordt het woord doormidden gehakt.
+            recommendatiemaken(originelenaam, vergelijking, recomlist)
         else:
             recommendsvoordit = record[0][0]
             eindlocatie(recommendsvoordit, recomlist)
 
-    except mysql.connector.Error as error:
+    except mysql.connector.Error as error: #Print uit waar de error zit
         print("Failed to get record from MySQL table: {}".format(error))
 
 def start(originelenaam):
-    recomlist = []
-    originelenaam = ''.join(originelenaam)
-    vergelijking = originelenaam
-    getLaptopDetail(originelenaam, vergelijking, recomlist)
+    recomlist = []                          #Lijst van recommendaties voor het product
+    originelenaam = ''.join(originelenaam)  #Originelenaam was nog een tuple
+    vergelijking = originelenaam            #Zodat ik de originele naam kan aanpassen, zonder de naam te verliezen. Vergelijking is diegene die aangepast wordt
+    recommendatiemaken(originelenaam, vergelijking, recomlist)
 
 def beginstart():
-    cursor.execute('SELECT name FROM products')
+    cursor.execute('SELECT name FROM products') #Haalt alle product namen binnen
     resultaat = cursor.fetchall()
-    for i in resultaat:
+    for i in resultaat:                     #Met elk product naam gaat de code werken in start(i), i is een productnaam
         start(i)
     if (mySQLConnection.is_connected()):
         cursor.close()
         mySQLConnection.close()
 
-createtable()
-beginstart()
+createtable()                               #Eerst table
+beginstart()                                #Dan de code
